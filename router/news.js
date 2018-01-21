b3bb2456c146840d0a71afb5f9996a788d4d7067
haohome <wuhaoxcp@gmail.com>
2018-01-21 18:31:15 +0800

    获取新闻news数据

--- router/news.js
+++ router/news.js
@@ -1,7 +1,6 @@
 const pool = require('../pool');
 const express = require('express');
 let router = express.Router();
-
 router.get("/list/:pno",function(req,res){
   //获取页码
   let pno=parseInt(req.params.pno);
@@ -9,13 +8,20 @@ router.get("/list/:pno",function(req,res){
     totalSize:0,    //新闻总条数
     pageSize:5,     //页面显示条数
     pageCount:0,    //总页数
-    pno,            //当前页码
+    pno:pno,            //当前页码
     data:[],        //当前页新闻数据
   }
   pool.query("SELECT COUNT(*) AS c FROM mf_news",function(err,result){
     if(err)throw err;
     outPut.totalSize=result[0]['c'];
-    res.json(outPut);
+    outPut.pageCount=Math.ceil(outPut.totalSize/outPut.pageSize);
+    let start=(pno-1)*outPut.pageCount;
+    let count=outPut.pageSize;
+    pool.query("SELECT * FROM mf_news ORDER BY pubTime DESC LIMIT ?,?",[start,count],function(err,data){
+      if(err)throw err;
+      outPut.data=data;
+      res.json(outPut);
+    })
   })
 })
 module.exports = router;
